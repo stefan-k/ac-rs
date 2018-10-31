0b66df01eb29f2ca94d8212c10388bad00f46f8c
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-10-31 16:28:07 +0100

    trying state

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,18 @@
 //!
 
 // #![warn(missing_docs)]
-#![feature(integer_atomics, const_fn)]
+#![feature(integer_atomics)]
 #![feature(atomic_min_max)]
 #![feature(alloc_error_hook)]
 #![feature(allocator_api)]
 #![feature(deadline_api)]
+#![feature(const_fn)]
 
 // #[macro_use]
 // extern crate failure;
 extern crate libc;
 extern crate rand;
+extern crate state;
 
 mod allocator;
 // #[macro_use]
@@ -27,34 +29,49 @@ mod executor;
 
 use self::allocator::LimitedAllocator;
 use self::executor::run_with_limits;
+use state::LocalStorage;
 use std::alloc::System;
 use std::time::Duration;
 
 #[global_allocator]
 pub static GLOBAL: LimitedAllocator<System> = LimitedAllocator::new(System);
 
+static CONFIG: LocalStorage<Configuration> = LocalStorage::new();
+
+struct Configuration {
+    x1: f64,
+    x2: f64,
+    x3: f64,
+}
+
 #[inline(never)]
-fn dummy(a: f64, b: f64, c: f64) -> u64 {
+fn dummy(a: f64) -> u64 {
     // use rand::{thread_rng, Rng};
     // let mut rng = thread_rng();
     // let n: u64 = rng.gen_range(1, 100);
     // let v: Vec<f64> = Vec::with_capacity(50);
     let n = 0;
+    let c = CONFIG.get().x2;
+    let b = CONFIG.get().x3;
     let time = (a * b - c - (b - a) * a).abs() as u64 + n;
     std::thread::sleep(Duration::from_millis(time));
     time
 }
 
 fn main() {
+    CONFIG.set(|| Configuration {
+        x1: 10.0,
+        x2: 4.0,
+        x3: 2.0,
+    });
+
     let f = || {
-        let a = 10.0;
-        let b = 2.0;
-        let c = 4.0;
-        let t = dummy(a, b, c);
+        let a = CONFIG.get().x1;
+        let t = dummy(a);
         t
     };
 
-    let stats = run_with_limits(f, Duration::from_millis(96), 1000);
+    let stats = run_with_limits(f, Duration::from_millis(96), 10000);
     println!("{:?}", stats);
 
     // println!(
